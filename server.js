3dbce0fa3519d8dbdc429685f72c4aff94ddfa61
lissy <slissy513@gmail.com>
2021-06-15 14:23:49 -0400

    added routes and server code to 'server.js'

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-const express = require("express");
+const express = require('express');
 
+//express server
 const app = express();
-const PORT =process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+//listener
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
