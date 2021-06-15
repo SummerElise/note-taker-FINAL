71d4140412bc78d95921984b2fc86afab238cd4a
lissy <slissy513@gmail.com>
2021-06-15 14:33:31 -0400

    added line of code in server.js to load files located in public directory

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//handles data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 //routes
 require('./routes/apiRoutes')(app);
